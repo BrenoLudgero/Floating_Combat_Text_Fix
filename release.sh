236c6870e60d9aeb2d7e4a0d86df466ad1904e0a
BrenoLudgero <breno.ludgero@yahoo.com>
2025-12-27 12:43:29 -0300

    Created script to initiate packager process

---
+++ release.sh
@@ -0,0 +1,15 @@
+git fetch --tags --force
+
+LATEST_REMOTE_TAG=$(git describe --tags --abbrev=0)
+
+read -p "Enter new version tag (current: $LATEST_REMOTE_TAG): " NEW_TAG
+
+# Deletes the current tag locally, remotely and 
+# deletes the latest release if NEW_TAG matches LATEST_REMOTE_TAG
+if [ "$NEW_TAG" == "$LATEST_REMOTE_TAG" ]; then
+    git tag --delete "$LATEST_REMOTE_TAG"
+    gh release delete "$LATEST_REMOTE_TAG" -y
+fi
+
+git tag "$NEW_TAG"
+git push remote "$NEW_TAG"
